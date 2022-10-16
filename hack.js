9332b0ab2dc2fe61dc440734029aa3b3bd2f08a8
Po Hsuan Huang <huangiris@berkeley.edu>
2022-10-15 23:19:12 -0700

    direct to new page

--- hack.js
+++ hack.js
@@ -4,12 +4,13 @@ function take_user_input(e) {
 
     // Get the value from each input object in index.html
     var nickName = document.getElementById("nickname").value;
-    var milkType = document.getElementById("milk");
+    var milkType = document.querySelector("input[type='radio'][name=milk]:checked").value;
 
     // print msg sent to Spiderman in console 
     console.log('Hi use. ')
-    console.log(nickName + ' wants to connect with you, she drinks coffee with');
+    console.log(nickName + ' wants to connect with you, she drinks coffee with ' + milkType);
 
     // Alert your user that the text msg to Spiderman is sent
-    alert(nickName + " Your request has been sent to goHike!")
+    alert(nickName + ", thanks for submitting the form! " + "go Hike will prepare hikemate who also drinks " + milkType)
+    window.location.href="newpage.html";
 }
