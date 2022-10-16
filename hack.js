a1a47a97f05c94b37af4ad01382f0d9bdfac3e23
Po Hsuan Huang <huangiris@berkeley.edu>
2022-10-16 08:33:58 -0700

    edit Q

--- hack.js
+++ hack.js
@@ -8,12 +8,10 @@ function take_user_input(e) {
     var milkType = document.querySelector("input[type='radio'][name=milk]:checked").value;
 
 
-    // print msg sent to Spiderman in console 
     console.log('Hi use. ')
     console.log(nickName + ' wants to connect with you, she drinks coffee with ' + milkType);
 
-    // Alert your user that the text msg to Spiderman is sent
-    alert(nickName + ", thanks for submitting the form! " + "go Hike will prepare hikemate who also drinks " + milkType + " and with ex Number " + exNum)
+    alert(nickName + ", thanks for submitting the form! go Hike will prepare hikemate who also drinks " + milkType);
     window.location.href = "newpage.html";
 
     const settings = {
